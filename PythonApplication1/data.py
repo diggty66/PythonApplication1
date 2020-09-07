470983c7f6dfd85bd4ddc51676f5fdf1d0eab3f4
John Giles <46950400+diggty66@users.noreply.github.com>
2020-09-07 12:33:37 -0400

    Add project files.

---
+++ PythonApplication1/data.py
@@ -0,0 +1,25 @@
+﻿def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem',
+            'author':'John Giles',
+            'create date':'09/04/2020'
+            },
+            {
+            'id':2,
+            'title':'Article Two',
+            'body':'Lorem',
+            'author':'John Doe',
+            'create date':'09/04/2020'
+            },
+             {
+            'id':3,
+            'title':'Article Three',
+            'body':'Lorem',
+            'author':'Jane Doe',
+            'create date':'09/04/2020'
+            }
+        ]
+    return articles
